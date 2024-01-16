cf5347bfe2930ceac0500a18f5a0f32eb11c0dbf
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-01-16 23:55:47 +0100

    Add sbt-ci-release plugin

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -1,17 +1,9 @@
+import sbt.Def
 import sbt.Keys.*
-import sbt.{Credentials, Def, Path, fileToRichFile, toRepositoryName}
+import xerial.sbt.Sonatype.GitHubHosting
+import xerial.sbt.Sonatype.autoImport.sonatypeProjectHosting
 
 object Publishing {
-  private val loadCredentialsFromSystemPropsOrFile = credentials += {
-    val fromSystemProps = for {
-      user <- sys.props.get("scala_plugin_deps_user")
-      pwd  <- sys.props.get("scala_plugin_deps_password")
-    } yield Credentials(realm = null, host = "packages.jetbrains.team", userName = user, passwd = pwd)
-
-    fromSystemProps
-      .getOrElse(Credentials(Path.userHome / ".sbt" / ".scala_plugin_deps_credentials"))
-  }
-
   private val publishIfNotSnapshot = publish := {
     if (isSnapshot.value)
       sys.error("Do not publish snapshot versions. Set a tag 'vX.Y.Z' to proceed. Example: v3.20.2")
@@ -20,9 +12,8 @@ object Publishing {
   }
 
   val settings: Seq[Def.Setting[?]] = Seq(
-    publishTo     := Some("Scala Plugin Dependencies" at "https://packages.jetbrains.team/maven/p/scl/scala-plugin-deps"),
+    sonatypeProjectHosting := Some(GitHubHosting("JetBrains", "sbt-kotlin-plugin", "scala-developers@jetbrains.com")),
     versionScheme := Some("semver-spec"),
-    loadCredentialsFromSystemPropsOrFile,
     publishIfNotSnapshot,
   )
 }
