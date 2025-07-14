d1af886fb9c3c2c9c9f98e4a139015262a61e28b
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-07-14 14:20:11 +0200

    publish to sonatype using the new scheme (+update sbt and sbt-ci-release to the latest versions)
    Related:
    - https://github.com/sbt/sbt-ci-release?tab=readme-ov-file#how-do-i-publish-to-sonatype-central
    - https://github.com/xerial/sbt-sonatype?tab=readme-ov-file#sonatype-central-host
    
    The code was copied from sbt-structure plugin form the recent changes to publishing

--- build.sbt
+++ build.sbt
@@ -1,11 +1,28 @@
+ThisBuild / organization := "org.jetbrains.scala"
+
+// Optional but nice-to-have
+ThisBuild / organizationName     := "JetBrains"
+ThisBuild / organizationHomepage := Some(url("https://www.jetbrains.com/"))
+
+ThisBuild / licenses += ("MIT", url("https://opensource.org/license/mit/"))
+
+ThisBuild / homepage := Some(url("https://github.com/JetBrains/sbt-kotlin-plugin"))
+
+ThisBuild / developers := List(
+  Developer(
+    id    = "JetBrains",
+    name  = "JetBrains",
+    email = "scala-developers@jetbrains.com",
+    url   = url("https://github.com/JetBrains")
+  )
+)
+
 lazy val sbtKotlinPlugin = project.in(file("."))
   .enablePlugins(SbtPlugin)
   .settings(Scripted.settings)
   .settings(Publishing.settings)
   .settings(
-    name         := "sbt-kotlin-plugin",
-    organization := "org.jetbrains.scala",
-    licenses     += ("MIT", url("https://opensource.org/license/mit/")),
+    name := "sbt-kotlin-plugin",
 
     scalaVersion   := "2.12.20",
     scalacOptions ++= Seq("-deprecation", "-feature", "-Werror", "-Xlint", "-release", "8"),
--- project/Publishing.scala
+++ project/Publishing.scala
@@ -1,9 +1,12 @@
 import sbt.Keys.*
-import sbt.{Def, ScmInfo, url}
-import xerial.sbt.Sonatype.GitHubHosting
-import xerial.sbt.Sonatype.autoImport.{sonatypeProfileName, sonatypeProjectHosting}
+import sbt.internal.sona
+import sbt.librarymanagement.ivy.Credentials
+import sbt.{Def, DirectCredentials, ScmInfo, url}
 
 object Publishing {
+
+  val SonatypeRepoName = "Sonatype Nexus Repository Manager"
+
   private val publishIfNotSnapshot = publish := {
     if (isSnapshot.value)
       sys.error("Do not publish snapshot versions. Set a tag 'vX.Y.Z' to proceed. Example: v3.20.2")
@@ -12,16 +15,39 @@ object Publishing {
   }
 
   val settings: Seq[Def.Setting[?]] = Seq(
-    sonatypeProfileName := "org.jetbrains",
-    sonatypeProjectHosting := Some(GitHubHosting("JetBrains", "sbt-kotlin-plugin", "scala-developers@jetbrains.com")),
     versionScheme := Some("semver-spec"),
     publishIfNotSnapshot,
+    
+    // Central Portal configuration - sbt-ci-release 1.11.0 defaults to Central Portal
+    // No explicit sonatypeCredentialHost setting needed for Central Portal
+    
+    // Required for sbt plugins with Central Portal
+    sbtPluginPublishLegacyMavenStyle := false,
+    
     scmInfo := Some(
       ScmInfo(
         url("https://github.com/JetBrains/sbt-kotlin-plugin"),
         "scm:git:git@github.com:JetBrains/sbt-kotlin-plugin.git",
         "scm:git:git@github.com:JetBrains/sbt-kotlin-plugin.git"
       )
-    )
+    ),
+
+    // Overwrite/filter-out existing credentials
+    // Use copy of `sbt.internal.SysProp.sonatypeCredentalsEnv` but with custom environment variables
+    credentials := credentials.value.filter {
+      case c: DirectCredentials => c.realm != SonatypeRepoName
+      case _ => true
+    } ++ {
+      val env = sys.env.get(_)
+      for {
+        username <- env("SONATYPE_USERNAME_NEW")
+        password <- env("SONATYPE_PASSWORD_NEW")
+      } yield Credentials(
+        SonatypeRepoName,
+        sona.Sona.host,
+        username,
+        password
+      )
+    },
   )
 }
