3ab22cd700eb13667cc21ce423812c382d29b096
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-07-16 11:58:01 +0200

    remove workaround hacks for sonatype publishing and regroup sbt settings

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -1,11 +1,7 @@
 import sbt.Keys.*
-import sbt.internal.sona
-import sbt.librarymanagement.ivy.Credentials
-import sbt.{Def, DirectCredentials, ScmInfo, url}
+import sbt.{Def, ScmInfo, url}
 
 object Publishing {
-
-  private val SonatypeRepoName = "Sonatype Nexus Repository Manager"
 
   val settings: Seq[Def.Setting[?]] = Seq(
     versionScheme := Some("semver-spec"),
@@ -17,23 +13,5 @@ object Publishing {
         "scm:git:git@github.com:JetBrains/sbt-kotlin-plugin.git"
       )
     ),
-
-    // Overwrite/filter-out existing credentials
-    // Use copy of `sbt.internal.SysProp.sonatypeCredentalsEnv` but with custom environment variables
-    credentials := credentials.value.filter {
-      case c: DirectCredentials => c.realm != SonatypeRepoName
-      case _ => true
-    } ++ {
-      val env = sys.env.get(_)
-      for {
-        username <- env("SONATYPE_USERNAME_NEW")
-        password <- env("SONATYPE_PASSWORD_NEW")
-      } yield Credentials(
-        SonatypeRepoName,
-        sona.Sona.host,
-        username,
-        password
-      )
-    },
   )
 }
